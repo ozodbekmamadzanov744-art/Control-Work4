1ae4dd4b1768f44ddace17c6dba26a0e17dab00f
Озодбек <ozodbekmamadzanov744@gmail.com>
2026-05-09 15:25:09 +0600

    feat: implement stage 7 (bonus) - random poisoning on feed and injury on play events

--- src/FeedAction.java
+++ src/FeedAction.java
@@ -1,9 +1,20 @@
+import java.util.Random;
+
 public class FeedAction implements Action {
+    private final Random random = new Random();
+    private static final double POISON_CHANCE = 0.2;
+
     @Override
     public void apply(Cat cat) {
-        cat.setSatiety(cat.getSatiety() + cat.getIncreaseStep());
-        cat.setMood(cat.getMood() + cat.getIncreaseStep());
-        System.out.println("Вы покормили кота " + cat.getName());
+        if (random.nextDouble() < POISON_CHANCE) {
+            cat.setMood(cat.getMood() - 20);
+            cat.setHealth(cat.getHealth() - 20);
+            System.out.println("Кот " + cat.getName() + " отравился! Настроение и здоровье сильно упали ");
+        } else {
+            cat.setSatiety(cat.getSatiety() + cat.getIncreaseStep());
+            cat.setMood(cat.getMood() + cat.getIncreaseStep());
+            System.out.println("Вы покормили кота " + cat.getName());
+        }
     }
 
     @Override
--- src/PlayAction.java
+++ src/PlayAction.java
@@ -1,10 +1,21 @@
+import java.util.Random;
+
 public class PlayAction implements Action {
+    private final Random random = new Random();
+    private static final double INJURY_CHANCE = 0.2;
+
     @Override
     public void apply(Cat cat) {
-        cat.setMood(cat.getMood() + cat.getIncreaseStep());
-        cat.setHealth(cat.getHealth() + cat.getIncreaseStep());
-        cat.setSatiety(cat.getSatiety() - cat.getDecreaseStep());
-        System.out.println("Вы поиграли с котом по имени  " + cat.getName() + ", " + cat.getAge() + " лет");
+        if (random.nextDouble() < INJURY_CHANCE) {
+            cat.setMood(cat.getMood() - 20);
+            cat.setHealth(cat.getHealth() - 20);
+            System.out.println("Кот " + cat.getName() + " получил травму! Настроение и здоровье сильно упали ");
+        } else {
+            cat.setMood(cat.getMood() + cat.getIncreaseStep());
+            cat.setHealth(cat.getHealth() + cat.getIncreaseStep());
+            cat.setSatiety(cat.getSatiety() - cat.getDecreaseStep());
+            System.out.println("Вы поиграли с котом по имени  " + cat.getName() + ", " + cat.getAge() + " лет");
+        }
     }
 
     @Override
